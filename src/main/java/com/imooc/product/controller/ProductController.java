84e6d7c2cb362b0986963bc6043937d89d3ef968
wangpeng <wangpeng0717@sina.cn>
2019-11-10 14:02:32 +0800

    '基本信息'

---
+++ src/main/java/com/imooc/product/controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.imooc.product.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    @RequestMapping("/")
+    public void list() {
+
+    }
+}
---
+++ src/main/java/com/imooc/product/dataobject/ProductCategory.java
@@ -0,0 +1,10 @@
+package com.imooc.product.dataobject;
+
+import lombok.Data;
+
+import javax.persistence.Table;
+
+@Table(name = "PRODUCT_CATEGORY")
+@Data
+public class ProductCategory {
+}
