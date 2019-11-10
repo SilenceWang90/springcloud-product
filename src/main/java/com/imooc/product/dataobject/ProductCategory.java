8a82bdd0a9308d1ce11af5c3c3cdda8762078e8e
wangpeng <wangpeng0717@sina.cn>
2019-11-10 14:39:05 +0800

    'dao相关完成'

--- src/main/java/com/imooc/product/dataobject/ProductCategory.java
+++ src/main/java/com/imooc/product/dataobject/ProductCategory.java
@@ -3,10 +3,24 @@ package com.imooc.product.dataobject;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Table(name = "PRODUCT_CATEGORY")
 @Data
+@Entity
 public class ProductCategory {
+    @Id
+    @GeneratedValue
+    private Integer categoryId;
 
+    //类目名称
+    private String categoryName;
+    //类目编号
+    private Integer categoryType;
+
+    private String createTime;
+
+    private String updateTime;
 }
